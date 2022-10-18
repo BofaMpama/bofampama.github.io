51644e58b5ac24f84cf720ad443660fd4052ab5f
Bofa Mpama <mpamabofa@gmail.com>
2022-10-19 00:14:05 +0100

    added new files

---
+++ SCRIPT.js
@@ -0,0 +1,51 @@
+let scrollReveal = '50px';
+function openMenu(){
+    document.getElementById('menu').style.height = '100%';
+}
+function closeMenu(){
+    document.getElementById('menu').style.height = 0;
+}
+function openPicture1a(){
+    document.getElementById('picture1a').style.width = '100%';
+}
+function closePicture1a(){
+    document.getElementById('picture1a').style.width = 0;
+}
+function openPicture2a(){
+    document.getElementById('picture2a').style.width = '100%';
+}
+function closePicture2a(){
+    document.getElementById('picture2a').style.width = 0;
+}
+function openPicture3a(){
+    document.getElementById('picture3a').style.width = '100%';
+}
+function closePicture3a(){
+    document.getElementById('picture3a').style.width = 0;
+}
+function openPicture1(){
+    document.getElementById('picture1').style.width = '100%';
+}
+function closePicture1(){
+    document.getElementById('picture1').style.width = 0;
+}
+function openPicture2(){
+    document.getElementById('picture2').style.width = '100%';
+}
+function closePicture2(){
+    document.getElementById('picture2').style.width = 0;
+}
+function openPicture3(){
+    document.getElementById('picture3').style.width = '100%';
+}
+function closePicture3(){
+    document.getElementById('picture3').style.width = 0;
+}
+function openPicture4(){
+    document.getElementById('picture4').style.width = '100%';
+}
+function closePicture4(){
+    document.getElementById('picture4').style.width = 0;
+}
+let photoreveal = document.getElementById('sec2-photo-reveal');
+let textreveal = document.getElementById('about-reveal');
