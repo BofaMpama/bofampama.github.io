e244662435a01ad6cca4e0c5f2abaa66b4a3f490
Bofa Mpama <mpamabofa@gmail.com>
2025-07-01 13:22:37 +0100

    site update

--- script.js
+++ script.js
@@ -8,3 +8,36 @@ function closeSidebar() {
     sidebar.style.display = 'none';
 
 }
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry) 
+        if (entry.isIntersecting){
+            entry.target.classList.add('animateUpReveal');
+            entry.target.classList.add('animateDownReveal');
+            entry.target.classList.add('animateLeftReveal');
+            entry.target.classList.add('animateRightReveal');
+
+        }else {
+            entry.target.classList.remove('animateUpReveal');
+            entry.target.classList.remove('animateDownReveal');            entry.target.classList.remove('animateDownReveal');
+            entry.target.classList.remove('animateLeftReveal');
+            entry.target.classList.remove('animateRightReveal');
+
+
+        }
+    });
+});
+
+const animateUpElements = document.querySelectorAll(".animateUp");
+animateUpElements.forEach((el) => observer.observe(el));
+
+const animateDownElements = document.querySelectorAll(".animateDown");
+animateDownElements.forEach((el) => observer.observe(el));
+
+const animateLeftElements = document.querySelectorAll(".animateLeft");
+animateLeftElements.forEach((el) => observer.observe(el));
+
+const animateRightElements = document.querySelectorAll(".animateRight");
+animateRightElements.forEach((el) => observer.observe(el));
