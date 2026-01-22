33e6390928539891d729e30aabdddd00d8e47cfc
Bofa Mpama <mpamabofa@gmail.com>
2026-01-22 22:59:21 +0100

    site edit

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,19 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+export default {
+  preprocess: vitePreprocess(),
 
-export default config;
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
+
+    paths: {
+      base: '/bofampama',
+      assets: '/bofampama'
+    }
+  }
+};
